d3685fc15bbd652096ddde77ecd693d0bdedb3f4
ArtcalO <niyca14@gmail.com>
2023-06-04 13:44:34 +0200

    remove unused admin

--- api/admin.py
+++ api/admin.py
@@ -1,17 +1,9 @@
 from django.contrib import admin
 from .models import *
 
+admin.site.register(Personnel)
 admin.site.register(Niveau)
 admin.site.register(Section)
 admin.site.register(Classe)
-admin.site.register(Cour)
-admin.site.register(Chapitre)
-admin.site.register(Exercice)
-admin.site.register(Correction)
-admin.site.register(ReponseEleve)
 admin.site.register(Eleve)
-admin.site.register(Categorie)
-admin.site.register(CoursSpeciaux)
-admin.site.register(ChapitresCSP)
-admin.site.register(ExerciceCSP)
-admin.site.register(Abonnements)
+
